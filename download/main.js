7d6775803eec037d7fbfac9b1908a6db0248fe85
theepicflyer <xiejc2000@gmail.com>
2016-03-12 22:27:00 +0800

    Fixed download for real now

--- download/main.js
+++ download/main.js
@@ -2,6 +2,7 @@
 
 const electron = require('electron');
 const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 const fs = require('fs')
 var request = require('request')
@@ -14,16 +15,25 @@ var options = {
 request(options).pipe(fs.createWriteStream('bla.iso'));
 
 
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+var mainWindow = null;
 
-function createWindow () {
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1280, height: 720});
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
   // and load the index.html of the app.
   mainWindow.loadURL('file:///index.html');
 
@@ -37,25 +47,8 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', createWindow);
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
 });
 
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+process.on('error', function(err) {
+  console.log(err);
 });
